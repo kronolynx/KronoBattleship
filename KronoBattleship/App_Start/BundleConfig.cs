8e9a6f3430393df69339d33e38ef9bc09cd36da8
Johann Og <kronolynx@gmail.com>
2016-06-12 12:56:52 +0200

    Minify and compress

--- KronoBattleship/App_Start/BundleConfig.cs
+++ KronoBattleship/App_Start/BundleConfig.cs
@@ -8,18 +8,9 @@ namespace KronoBattleship
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/jquery-{version}.js",
+                      "~/Scripts/jquery.validate*",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js",
                       "~/Scripts/ChatScript.js"));
@@ -28,6 +19,8 @@ namespace KronoBattleship
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
                       "~/Content/Site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
